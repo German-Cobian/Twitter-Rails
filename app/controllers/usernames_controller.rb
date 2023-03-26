6af2e47ae817eaab7394acbdce6579a8a244e48f
German Renato Cobian <germancobian@hotmail.com>
2023-03-26 16:55:19 -0700

    Tests for username

--- app/controllers/usernames_controller.rb
+++ app/controllers/usernames_controller.rb
@@ -7,6 +7,13 @@ class UsernamesController < ApplicationController
   end
 
   def update
-    
+    current_user.update(username_params)
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def username_params
+    params.require(:user).permit(:username)
   end
 end
