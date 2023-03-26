86f1efa8f26bc98f691afa3cc4387e4249ae8449
German Renato Cobian <germancobian@hotmail.com>
2023-03-26 15:36:44 -0700

    Set up controllers, routes and views for username

---
+++ app/controllers/usernames_controller.rb
@@ -0,0 +1,11 @@
+class UsernamesController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    
+  end
+
+  def update
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   root "home#index"
   resources :tweets, only: :create
   get :dashboard, to: "dashboard#index"
+  resources :usernames, only: [:new, :update]
 end
