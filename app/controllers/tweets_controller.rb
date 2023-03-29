aedd2c4e1102b8728577cd2fb7278ec4f264851c
German Renato Cobian <germancobian@hotmail.com>
2023-03-29 16:06:41 -0700

    Implement form submission with turbo

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,10 +2,13 @@ class TweetsController < ApplicationController
   before_action :authenticate_user!
   
   def create
-    tweet = Tweet.new(tweet_params.merge(user: current_user))
+    @tweet = Tweet.new(tweet_params.merge(user: current_user))
 
-    if tweet.save
-      redirect_to dashboard_path
+    if @tweet.save
+      respond_to do |format|
+        format.html { redirect_to dashboard_path }
+        format.turbo_stream
+      end  
     end
   end
 
