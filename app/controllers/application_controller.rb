f9ac4510eca794bae162a6f25076a3a91ea4ea94
German Renato Cobian <germancobian@hotmail.com>
2023-04-20 14:06:22 -0700

    Correction so that bookmarks work correctly in turbo from tweets/show (they do not from tweets/tweet page)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,18 +10,4 @@ class ApplicationController < ActionController::Base
     redirect_to new_username_path
   end
 
-  def previous_path
-    @previous_path ||= Rails.application.routes.recognize_path(request.referrer)
-  end
-
-  def previous_controller_name
-    previous_path[:controller]
-  end
-
-  helper_method :previous_controller_name
-
-  def previous_action_name
-    previous_path[:action]
-  end
-  helper_method :previous_action_name
 end
