5a7ebce70fc977506430f24ec4e2a2edf09cc187
German Renato Cobian <germancobian@hotmail.com>
2023-04-19 18:01:36 -0700

    Create 'previous' methods in application controler and test (in bookmarks controller create action) that it logs the path back to console

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,16 @@ class ApplicationController < ActionController::Base
   def redirect_to_username_form
     redirect_to new_username_path
   end
+
+  def previous_path
+    @previous_path ||= Rails.application.routes.recognize_path(request.referrer)
+  end
+
+  def previous_controller_name
+    previous_path[:controller]
+  end
+
+  def previous_action_name
+    previous_path[:action]
+  end
 end
