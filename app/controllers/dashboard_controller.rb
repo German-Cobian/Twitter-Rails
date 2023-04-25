419bc65bf59ff0458e2e0254310bc16196cf7a46
German Renato Cobian <germancobian@hotmail.com>
2023-04-25 13:28:35 -0700

    Rendering of the reply tweets in the show view of each particular tweet

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,10 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @tweets = Tweet.includes(:user, :liked_users, :bookmarked_users).order(created_at: :desc).map do |tweet|
-      TweetPresenter.new(tweet: tweet, current_user: current_user)
+    @tweets = Tweet.includes(
+      :user, :liked_users, :bookmarked_users
+      ).order(created_at: :desc).map do |tweet|
+        TweetPresenter.new(tweet: tweet, current_user: current_user)
     end
   end
 end
